117c2a6eb18cf7c4d64d1321299cd9a4d4c1801a
Rark7040 <rarkhopper@gmail.com>
2024-12-09 06:46:07 +0000

    ホットリロード、litでのコンポーネントレンダリング対応

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './styles.css';
+import { render } from 'lit-html';
+import { Footer, Header } from '../assets/js/components/common';
 
 if (module.hot) {
   module.hot.accept();
 }
+
+function renderComponent(compoent, domId) {
+  const el = document.getElementById(domId);
+  
+  if (null === el) {
+    return;
+  }
+
+  render(compoent, el);
+}
+
+// 各コンポーネントを描画
+renderComponent(Header(), 'header');
+renderComponent(Footer(), 'footer');
