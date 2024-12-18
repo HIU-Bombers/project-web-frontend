da8b64eadb3b20cb07d3f8a880caba8e831088b3
rito528 <39003544+rito528@users.noreply.github.com>
2024-12-18 13:23:11 +0000

    feat: QR コード生成用エンドポイントの実装

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const QRCode = require('qrcode');
 const app = express();
 
 function getSessionId(req) {
@@ -8,7 +9,7 @@ function getSessionId(req) {
 }
 
 function requireLogin(req, res, next) {
-  if (null === getSessionId(req)) {    
+  if (null === getSessionId(req)) {
     res.redirect('/login');
     return;
   }
@@ -43,13 +44,13 @@ app.use('/', express.static('views'));
 // api
 app.post('/logout', async (req, res) => {
   res.clearCookie('PROJECT_BASICS__SESSION_ID');
-  
+
   // const signoutRes = await fetch("http://localhost:9000/signout",{
   //   method: "POST",
   //   mode: 'cors',
   //   credentials: 'include',
   // });
-  
+
   // console.log(signoutRes.status);
   // console.log(signoutRes.headers);
   // console.log(signoutRes.body);
@@ -66,6 +67,24 @@ app.post('/sessioncheck', async (req, res) => {
   res.json({
     "isLoggedIn": null !== getSessionId(req)
   })
+});
+
+app.get('/qrcode-gen/:token', async (req, res) => {
+  let token = req.params.token;
+
+  let QRbase64 = await new Promise((resolve, reject) => {
+    QRCode.toDataURL(`http://localhost:3000/use-ticket/${token}`, function (err, code) {
+      if (err) {
+        reject(reject);
+        return;
+      }
+      resolve(code);
+    });
+  });
+
+  res.json({
+    "base64-qr-code": QRbase64
+  });
 });
 
 app.listen(3000, () => {
